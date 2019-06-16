87ccf64dc373e8bf59ead97a6ad2c44d8ac1ae27
Estrella Cholod <estrellangie19@gmail.com>
2019-06-15 22:44:35 -0300

    Inicio un poco mas lindo

--- Code/Web/country-web/src/App.js
+++ Code/Web/country-web/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import Login from "./componente/login/Login";
+import Encabezado from "./componente/Encabezado/Encabezado";
+import "./estilo.css";
+import logo1 from "./logo1.jpg";
 
+// para probar encabezado poner <Encabezado></Encabezado>
+// <h1 className="display-3 text-center">Come in </h1>
 
 class App extends Component{
   render(){
     return(
-      <div className="app container">
-        <div className="jumbotron">
-          <p className="lead text-center">Come in </p>
+      <div className="app container-fluid ">        
+        <div className="jumbotron  "> 
+        <Encabezado></Encabezado>
+          <div className="text-center">     
+          <img src={logo1} width="300" height="129"></img>
+          </div>  
+          <hr className="my-4"></hr>
         </div>
         <Login ></Login>
+        
       </div>
 
 
