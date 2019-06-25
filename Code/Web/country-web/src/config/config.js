2c3a0b3e09e718d8f194979e545fb789419fc9de
luis096 <luis268quiroga@gmail.com>
2019-06-25 04:27:57 -0300

    Login-25/06

---
+++ Code/Web/country-web/src/config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyB837_BMh2WnGbXWVoOE0xclKymC7aC2lc",
+    authDomain: "countryapp-ab360.firebaseapp.com",
+    databaseURL: "https://countryapp-ab360.firebaseio.com",
+    projectId: "countryapp-ab360",
+    storageBucket: "",
+    messagingSenderId: "570428474188",
+    appId: "1:570428474188:web:9865d0ea7c0a9e0b"
+  }; 
