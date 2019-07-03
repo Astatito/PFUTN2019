7c0d08827f4cf8e1011dfd7238a840ee07c316e9
alexispagura <alexis_pagura_80@hotmail.com>
2019-07-03 18:58:09 -0300

    Componente Login Mobile

--- Code/Mobile/countryApp/src/Components/App.js
+++ Code/Mobile/countryApp/src/Components/App.js
@@ -1,59 +1,12 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import {Field, Header, Card, CardSection, Button} from './Common';
-import Firebase from './Firebase';
+import Login from './Main/Login';
+
 class App extends Component {
-
-    state = {email: '', password: '', result: ''} ;
-
-
-
-    onButtonPress() {
-        Firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-            .then(() => {
-                this.setState({result: "Logueo exitoso."})
-            })
-            .catch(() => {
-                this.setState({result: "Falló la autenticación."})
-            })
-    }
 
     render() {
         return (
-            <View>
-                <Header headerText="Welcome to CountryApp!"/>
-
-                <Card>
-                    <CardSection>
-                        <Field
-                            placeholder="ejemplo@mail.com"
-                            label="Email"
-                            value={this.state.email}
-                            onChangeText={(email) => this.setState({email})}
-                            hidden={false}
-                        />
-                    </CardSection>
-                    <CardSection>
-                        <Field
-                            placeholder="password"
-                            label="Password"
-                            value={this.state.password}
-                            onChangeText={(password) => this.setState({password})}
-                            hidden={true}
-                        />
-                    </CardSection>
-                    <CardSection>
-                        <Button onPress={this.onButtonPress.bind(this)}>
-                            Log in
-                        </Button>
-                    </CardSection>
-                    <CardSection>
-                        <Text>{this.state.result}</Text>
-                    </CardSection>
-                </Card>
-            </View>
+            <Login></Login>
         );
     }
 }
-
 export default App;
---
+++ Code/Mobile/countryApp/src/Components/Main/Login.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import {Field, Header, Card, CardSection, Button} from '../Common';
+import Firebase from '../Firebase'
+
+class Login extends Component {
+
+    state = {email: '', password: '', result: ''} ;
+    onButtonPress() {
+        Firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => {
+                this.setState({result: "Logueo exitoso."})
+            })
+            .catch(() => {
+                this.setState({result: "Falló la autenticación."})
+            })
+    }
+    render() {
+        return (
+            <View>
+                <Header headerText="Welcome to CountryApp!"/>
+                <Card>
+                    <CardSection>
+                        <Field
+                            placeholder="ejemplo@mail.com"
+                            label="Email"
+                            value={this.state.email}
+                            onChangeText={(email) => this.setState({email})}
+                            hidden={false}
+                        />
+                    </CardSection>
+                    <CardSection>
+                        <Field
+                            placeholder="password"
+                            label="Password"
+                            value={this.state.password}
+                            onChangeText={(password) => this.setState({password})}
+                            hidden={true}
+                        />
+                    </CardSection>
+                    <CardSection>
+                        <Button onPress={this.onButtonPress.bind(this)}>
+                            Log in
+                        </Button>
+                    </CardSection>
+                    <CardSection>
+                        <Text>{this.state.result}</Text>
+                    </CardSection>
+                </Card>
+            </View>
+        );
+    }
+}
+export default Login;
