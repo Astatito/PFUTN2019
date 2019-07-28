b2f197ba4aa7758d03b7dae7e58982b77485114b
alexispagura <alexis_pagura_80@hotmail.com>
2019-07-27 22:12:50 -0300

    no message

--- Code/Mobile/countryApp/src/Components/App.js
+++ Code/Mobile/countryApp/src/Components/App.js
@@ -3,10 +3,6 @@ import Login from './Main/Screens/Login';
 import EncargadoNavigation from './Navigation/EncargadoNavigation';
 import Propietario from './Main/Screens/Propietario';
 import {createSwitchNavigator,createAppContainer} from 'react-navigation';
-import IngresoManual from './Main/Screens/IngresoManual';
-import EgresoManual from './Main/Screens/EgresoManual';
-import Ingreso from './Main/Screens/Ingreso';
-import Egreso from './Main/Screens/Egreso';
 
 class App extends Component {
     render() {
@@ -22,13 +18,9 @@ export default App
 // La que se encuentra primero es la que se ejecuta.
 
 const AppSwitchNavigator = createSwitchNavigator({
+    Login: {screen: Login},
     Encargado: { screen: EncargadoNavigation },  
     Propietario: {screen: Propietario},  
-    Login: {screen: Login},
-    IngresoManual: {screen: IngresoManual},
-    EgresoManual: {screen: EgresoManual},
-    Ingreso : {screen: Ingreso},
-    Egreso: {screen: Egreso}
   });
 
 const AppContainer = createAppContainer(AppSwitchNavigator);
