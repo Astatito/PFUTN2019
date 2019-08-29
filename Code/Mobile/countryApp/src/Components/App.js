5946a9ce5e36d41e7bb1a1991fc235ead159012c
alexispagura <alexis_pagura_80@hotmail.com>
2019-08-28 22:37:22 -0300

    Mejoras en la pantalla de Login

--- Code/Mobile/countryApp/src/Components/App.js
+++ Code/Mobile/countryApp/src/Components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import {View} from 'react-native'
 import Login from './Main/Screens/Login';
 import EncargadoNavigation from './Navigation/EncargadoNavigation';
 import PropietarioNavigation from './Navigation/PropietarioNavigation';
 import RegistrarVisitante from './Main/Screens/RegistroVisitante';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+
 class App extends Component {
     render() {
         return <AppContainer />;
@@ -14,10 +16,11 @@ export default App;
 // La que se encuentra primero es la que se ejecuta.
 
 const AppSwitchNavigator = createSwitchNavigator({
+    Login: { screen: Login},
     RegistrarVisitante: {screen: RegistrarVisitante },
     Encargado: { screen: EncargadoNavigation },
     Propietario: { screen: PropietarioNavigation },
-    Login: { screen: Login },
+    
     
 });
 
